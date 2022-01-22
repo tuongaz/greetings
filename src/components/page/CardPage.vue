f8eca57249fdcc5987fdd0df80da1dabb9003f94
Tuong Le <tuongaz@gmail.com>
2022-01-23 10:38:11 +1100

    wip

--- src/components/page/CardPage.vue
+++ src/components/page/CardPage.vue
@@ -2,11 +2,19 @@
   <Header />
   <Body>
     <div>
+      <h2>A card for John Doe</h2>
       <Card :activePageId="1" />
     </div>
   </Body>
   <Footer />
 </template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/mixins.scss';
+h2 {
+  text-align: center;
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
