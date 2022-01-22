20c753ff09915bbe0efad83c705d52d7ca25e108
Tuong Le <tuongaz@gmail.com>
2022-01-23 10:06:16 +1100

    wip

--- src/components/page/CardPage.vue
+++ src/components/page/CardPage.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Card from '@/components/greeting/Card.vue';
+import Card from '@/components/card/Card.vue';
 
 export default defineComponent({
   components: {
