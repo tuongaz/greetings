ae97761f08d7324885402a4e6c44e948948ff751
Tuong Le <tuongaz@gmail.com>
2022-02-06 18:27:40 +1100

    nice

---
+++ src/components/form/BaseInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <label>{{ label }}</label>
+  <input
+    :placeholder="label"
+    class="field"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    label: {
+      required: true,
+      type: String
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+});
+</script>
--- src/components/page/SetupPage.vue
+++ src/components/page/SetupPage.vue
@@ -1,9 +1,33 @@
 <template>
   <Header />
   <Body>
-    <form>
+    <form @submit.prevent="submit()">
+      <BaseInput label="Name" v-model="card.name" />
       <button>Submit</button>
     </form>
   </Body>
   <Footer />
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from '@/components/page/Header.vue';
+import Footer from '@/components/page/Footer.vue';
+import Body from '@/components/page/Body.vue';
+import BaseInput from '@/components/form/BaseInput.vue';
+
+export default defineComponent({
+  components: { Body, Header, Footer, BaseInput },
+  data() {
+    return {
+      card: {
+        name: 'hello world'
+      }
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.card);
+    }
+  }
+});
+</script>
